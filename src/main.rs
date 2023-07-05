40eabb4474496181aade325131f98531baa4a477
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-07-05 12:00:12 -0400

    Built command with options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::process::Command;
+
+fn build_command() -> Command {
+    let mut command = Command::new("ps");
+    command.args(&["-e", "-o", "pid,drs", "--sort", "-drs"]);
+
+    command
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut command = build_command();
+    let output = command.output().expect("failed to execute process");
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
 }
