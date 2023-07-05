1fe5c9329f295697540ecc25065e5e71655edee3
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-07-05 12:39:02 -0400

    Command output can now be converted to a HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::process::{Command, Output};
+use std::{
+    collections::HashMap,
+    process::{Command, Output},
+};
 
 struct ProcessCommand {
     command: Command,
@@ -15,10 +18,27 @@ impl ProcessCommand {
     fn execute(&mut self) -> Output {
         self.command.output().expect("failed to execute process")
     }
+
+    fn convert_output_to_map(&mut self) -> HashMap<String, String> {
+        let output = self.execute();
+        let output = String::from_utf8_lossy(&output.stdout);
+        let mut map = HashMap::new();
+
+        for line in output.lines() {
+            let mut iter = line.split_whitespace();
+            let pid = iter.next().unwrap();
+            let drs = iter.next().unwrap();
+
+            map.insert(pid.to_string(), drs.to_string());
+        }
+
+        map
+    }
 }
 
 fn main() {
     let mut process_command = ProcessCommand::new();
-    let output = process_command.execute();
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    let map = process_command.convert_output_to_map();
+
+    println!("{:?}", map);
 }
