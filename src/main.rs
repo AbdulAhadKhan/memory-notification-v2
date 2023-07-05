bebd1b315a0e72c8379b1f923088e3a69253f231
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-07-05 12:19:31 -0400

    PS is now in a struct with a function wrapper for execution

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
-use std::process::Command;
+use std::process::{Command, Output};
 
-fn build_command() -> Command {
-    let mut command = Command::new("ps");
-    command.args(&["-e", "-o", "pid,drs", "--sort", "-drs"]);
+struct ProcessCommand {
+    command: Command,
+}
 
-    command
+impl ProcessCommand {
+    fn new() -> Self {
+        let mut command = Command::new("ps");
+        command.args(&["-e", "-o", "pid,drs", "--sort", "-drs"]);
+
+        Self { command }
+    }
+
+    fn execute(&mut self) -> Output {
+        self.command.output().expect("failed to execute process")
+    }
 }
 
 fn main() {
-    let mut command = build_command();
-    let output = command.output().expect("failed to execute process");
+    let mut process_command = ProcessCommand::new();
+    let output = process_command.execute();
     println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
 }
