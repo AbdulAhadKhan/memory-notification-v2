9538168d35185d37c3091c9c48e1e9ee05d16609
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-07-05 15:27:07 -0400

    Dirty implementation of config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,47 @@
 mod process_command;
 use process_command::ProcessCommand;
-use std::{thread, time};
+use std::{fs, thread, time};
+use toml::Value;
+
+fn retrieve_config() -> Value {
+    let config = fs::read_to_string("config.toml").expect("Unable to read config file");
+    config
+        .parse::<Value>()
+        .expect("Unable to parse config file")
+}
+
+fn get_value_or_default<'a>(map: &'a Value, module: &str, key: &str, default: &'a str) -> &'a str {
+    let test = map
+        .get(module)
+        .and_then(|server| {
+            server.get(key).and_then(|value| {
+                if value.is_integer() {
+                    Some(value.as_integer().unwrap().to_string())
+                } else if value.is_str() {
+                    Some(value.as_str().unwrap().to_string())
+                } else {
+                    None
+                }
+            })
+        })
+        .unwrap_or(default.to_string());
+
+    println!("{}", test);
+    "hmm"
+}
 
 fn main() {
-    loop {
-        let mut process_command = ProcessCommand::new();
-        let map = process_command.convert_output_to_map();
+    let config = retrieve_config();
+    let interval = get_value_or_default(&config, "core", "interval", "1");
 
-        println!("{:?}", map);
+    println!("{}", interval);
 
-        thread::sleep(time::Duration::from_secs(1));
-    }
+    // loop {
+    //     let mut process_command = ProcessCommand::new();
+    //     let map = process_command.convert_output_to_map();
+
+    //     println!("{:?}", map);
+
+    //     thread::sleep(time::Duration::from_secs(1));
+    // }
 }
