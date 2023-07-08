d777c23a37ce49738c5eae3f7279ad2c9d24bea9
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-07-08 19:58:11 -0400

    Moved process observer into its own module

---
+++ src/policies/process_observer.rs
@@ -0,0 +1,38 @@
+use crate::policies::cliff_queue::CliffQueue;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct ProcessObserver {
+    processes: HashMap<u32, CliffQueue<u64>>,
+    window_size: usize,
+}
+
+pub trait ProcessObserverTrait {
+    fn update_processes(&mut self, processes: HashMap<u32, u64>);
+}
+
+impl ProcessObserver {
+    pub fn new(window_size: usize) -> Self {
+        let processes = HashMap::new();
+        Self {
+            processes,
+            window_size,
+        }
+    }
+}
+
+impl ProcessObserverTrait for ProcessObserver {
+    fn update_processes(&mut self, processes: HashMap<u32, u64>) {
+        self.processes.retain(|pid, _| processes.contains_key(pid));
+        for (pid, cpu_usage) in processes {
+            if !self.processes.contains_key(&pid) {
+                let mut queue = CliffQueue::new(self.window_size);
+                queue.populate(0);
+                queue.push(cpu_usage);
+                self.processes.insert(pid, queue);
+            } else {
+                self.processes.get_mut(&pid).unwrap().push(cpu_usage);
+            }
+        }
+    }
+}
