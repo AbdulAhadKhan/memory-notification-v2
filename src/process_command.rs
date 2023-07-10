e84509f330cf0e018989e4880f26515583a87464
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-07-10 15:57:38 -0400

    Removed unused code pieces

--- src/process_command.rs
+++ src/process_command.rs
@@ -5,11 +5,10 @@ use std::{
 
 pub struct ProcessCommand {
     command: Command,
-    lower_limit: u64,
 }
 
 impl ProcessCommand {
-    pub fn new(lower_limit: u64) -> Self {
+    pub fn new() -> Self {
         let mut command = Command::new("ps");
 
         #[cfg(target_os = "macos")]
@@ -18,10 +17,7 @@ impl ProcessCommand {
         #[cfg(target_os = "linux")]
         command.args(&["-e", "-o", "pid,drs", "--sort", "-drs"]);
 
-        Self {
-            command,
-            lower_limit,
-        }
+        Self { command }
     }
 
     pub fn execute(&mut self) -> Output {
