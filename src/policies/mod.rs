336de8b76b7fec0fc07a2ca1c240aaf2cae37aa0
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-07-17 16:27:04 -0400

    Moving util functions to their own module

--- src/policies/mod.rs
+++ src/policies/mod.rs
@@ -7,33 +7,6 @@ use process_observer::ProcessObserver;
 pub use process_observer::ProcessObserverTrait;
 use std::{fs, io};
 
-fn log_to_file(file_name: &str, message: &str) {
-    use fs::OpenOptions;
-    use io::Write;
-
-    let mut file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(file_name)
-        .unwrap();
-
-    if let Err(e) = writeln!(file, "{}", message) {
-        eprintln!("Couldn't write to file: {}", e);
-    }
-}
-
-fn build_string(violations: Vec<(u32, u64)>) -> String {
-    let mut message = String::new();
-    let time = chrono::Utc::now();
-
-    message.push_str(&format!("\n[UTC TIMESTAMP: {}]\n", time));
-
-    for (pid, cpu_usage) in violations {
-        message.push_str(&format!("PID: {: <8}\tMEM: {}\n", pid, cpu_usage));
-    }
-    message
-}
-
 pub fn p1_log_on_lower(processes: &ProcessObserver) {
     let lower_limit = CONFIGS.core.lower_limit;
     let file_name = &CONFIGS.policy_configs.p1_log_file;
---
+++ src/policies/utils.rs
@@ -0,0 +1,26 @@
+fn log_to_file(file_name: &str, message: &str) {
+    use fs::OpenOptions;
+    use io::Write;
+
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(file_name)
+        .unwrap();
+
+    if let Err(e) = writeln!(file, "{}", message) {
+        eprintln!("Couldn't write to file: {}", e);
+    }
+}
+
+fn build_string(violations: Vec<(u32, u64)>) -> String {
+    let mut message = String::new();
+    let time = chrono::Utc::now();
+
+    message.push_str(&format!("\n[UTC TIMESTAMP: {}]\n", time));
+
+    for (pid, cpu_usage) in violations {
+        message.push_str(&format!("PID: {: <8}\tMEM: {}\n", pid, cpu_usage));
+    }
+    message
+}
