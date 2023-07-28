3ff952387ff6fcc48620bee54da0ee2b1aecb2b0
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-07-28 10:15:51 -0400

    Bug fix in file path

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ pub static CONFIGS: Lazy<Config> = Lazy::new(|| {
         Some(config) => Config::new(&config),
         None => {
             let home = home::home_dir().expect("Unable to get home directory");
-            let config_path = home.join(".config").join("msnv2").join("config.toml");
+            let config_path = home.join(".config").join("mnsv2").join("config.toml");
             Config::new(config_path.to_str().unwrap())
         }
     }
