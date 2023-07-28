6ba779f2c712ba04ffbff44bae4417535da4d969
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-07-28 10:13:12 -0400

    Moving config to .config or user defined path

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,36 @@ mod email;
 mod policies;
 mod process_command;
 
+use clap::Parser;
 use config_parser::Config;
+use home;
 use once_cell::sync::Lazy;
 use policies::{process_observer, run_policies, ProcessObserverTrait};
 use process_command::ProcessCommand;
 use std::{thread, time};
 
-pub static CONFIGS: Lazy<Config> = Lazy::new(|| Config::new());
+#[derive(Parser, Debug)]
+#[command(version, about, author)]
+struct Opts {
+    #[clap(short, long)]
+    config: Option<String>,
+}
+
+pub static CONFIGS: Lazy<Config> = Lazy::new(|| {
+    let opts: Opts = Opts::parse();
+    match opts.config {
+        Some(config) => Config::new(&config),
+        None => {
+            let home = home::home_dir().expect("Unable to get home directory");
+            let config_path = home.join(".config").join("msnv2").join("config.toml");
+            Config::new(config_path.to_str().unwrap())
+        }
+    }
+});
 
 fn main() {
+    println!("{}", home::home_dir().unwrap().display());
+
     let mut process_observer =
         process_observer::ProcessObserver::new(CONFIGS.core.observation_window);
 
