131f79b5379f6572cc75d66da8498d42f7befead
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-07-30 05:52:31 -0400

    Added struct for attachment info, threaded send email method, and consequential refactors

--- src/email/mod.rs
+++ src/email/mod.rs
@@ -5,9 +5,23 @@ use lettre::message::{header::ContentType, MultiPart};
 use lettre::message::{Attachment, SinglePart};
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
-use std::fs;
+use std::{fs, option::Option, thread};
 
-pub struct EmailHandler<'a> {
+pub struct AttachmentInfo {
+    filename: String,
+    mime_type: String,
+}
+
+impl AttachmentInfo {
+    pub fn new(filename: &str, mime_type: &str) -> Self {
+        Self {
+            filename: String::from(filename),
+            mime_type: String::from(mime_type),
+        }
+    }
+}
+
+struct EmailHandler<'a> {
     email_configs: &'a Email,
     subject: String,
     message: String,
@@ -24,11 +38,7 @@ impl EmailHandler<'_> {
         }
     }
 
-    pub fn append_message(&mut self, message: &str) {
-        self.message.push_str(message);
-    }
-
-    pub fn add_attachment(&mut self, filename: &str, content_type: &str) {
+    fn add_attachment(&mut self, filename: &str, content_type: &str) {
         let contents = fs::read(filename).expect("Something went wrong reading the file");
         let content_type = ContentType::parse(content_type).unwrap();
         self.attachment =
@@ -57,7 +67,7 @@ impl EmailHandler<'_> {
             .unwrap()
     }
 
-    pub fn send_email(&self) {
+    fn send_email(&self) {
         let credentials = Credentials::new(
             self.email_configs.email.to_string(),
             self.email_configs.password.to_string(),
@@ -74,4 +84,14 @@ impl EmailHandler<'_> {
             Err(e) => panic!("Could not send email: {:?}", e),
         }
     }
+}
+
+pub fn send_email(subject: &str, message: &str, attachment: Option<AttachmentInfo>) {
+    let mut email_handler = EmailHandler::new(subject, message);
+    if let Some(attachment) = attachment {
+        email_handler.add_attachment(&attachment.filename, &attachment.mime_type);
+    }
+    thread::spawn(move || {
+        email_handler.send_email();
+    });
 }
